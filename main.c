e6d61de9dd1bb26a7b5ebb6c22cee5c782c02177
cdodde <74382206+cdodde@users.noreply.github.com>
2025-04-18 09:52:03 -0400

    Update main.c

--- main.c
+++ main.c
@@ -25,6 +25,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include "sine_table.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,15 +49,24 @@ DAC_HandleTypeDef hdac1;
 
 SD_HandleTypeDef hsd1;
 
-/* USER CODE BEGIN PV */
+TIM_HandleTypeDef htim1;
 
+UART_HandleTypeDef huart3;
+
+/* USER CODE BEGIN PV */
+int16_t size = 2048; // size of buffer;
+int16_t buffer_A[2048];
+int16_t buffer_B[2048];
+int timer6flag = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SDMMC1_SD_Init(void);
+static void MX_USART3_UART_Init(void);
 static void MX_DAC1_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -63,6 +74,14 @@ static void MX_DAC1_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 // find data chunk offset
+uint32_t getfreq(FIL* file){
+	UINT count0;
+	uint32_t freq[1];
+	f_lseek(file, 24);
+	f_read(file, freq, 4, &count0);
+	return freq[0];
+}
+
 uint32_t find_data_offset(FIL *file) {
     uint8_t buffer[12];
     UINT bytes_read;
@@ -80,31 +99,127 @@ uint32_t find_data_offset(FIL *file) {
         }
     }
 }
-
-
-
+/*
 void playwav(FIL* fp,FRESULT res,uint8_t index){
 	UINT count;
 	char filename[32];
-	int16_t size = 2048; // size of buffer;
-	int16_t buffer_A[size];
-	int16_t buffer_B[size];
 
-	sprintf(filename, "%utest.wav", index);
-	res = f_open(fp, filename, FA_READ);
+	//sprintf(filename, "tone.wav", index);
+	res = f_open(fp, "testClash.wav", FA_READ);
   	   if(res == FR_OK){
+  		   uint32_t freq = getfreq(fp);
   		   f_lseek(fp, 4096*6);
   		   //UINT offset;
   		   //offset = find_data_offset(fp);
   		   //f_lseek(fp, offset);// jump to the end of header  // replaced by auto omit first 4096 bytes
-  		   for(int i = 1; i<5;i++){
-           f_read(fp, buffer_A, 4096, &count);
+  		   HAL_TIM_Base_Start_IT(&htim1);
+  		   HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
+  		   f_read(fp, buffer_A, 4096, &count);
+  		   for(int buff_index = 0; buff_index<100;buff_index++){
+           f_read(fp, buffer_B, 4096, &count);
+           int j = 0;
+           while(1){
+           	   if(timer6flag && j<size){
+           			   uint16_t dac_val = (uint16_t)((buffer_A[j] + 32768) >> 4); // Convert 16-bit PCM to 12-bit unsigned
+           			   HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, dac_val);
+           			   timer6flag = 0;
+           			   ++j;
+           	   }
+           	   else if(j>=size){
+           		   bufferflag = 1;
+           		   break;
+           	  }
+          }
+  		  }
+          f_close(fp);
+  	  }
+}
+*/
 
-  		   }
+uint32_t getsize(FIL* file){
+	UINT count0;
+	uint32_t size[1];
+	f_lseek(file, 4);
+	f_read(file, size, 4, &count0);
+	return size[0];
+}
 
-  	  	   f_close(fp);
-  	   }
+void playwav(FIL* fp, FRESULT res, uint8_t index) {
+    UINT count;
+    char filename[32];
+    res = f_open(fp, "testClash.wav", FA_READ);
+    //testClash.wav
 
+    if(res == FR_OK) {
+        uint32_t filesize = getsize(fp);
+        f_lseek(fp, 4096); // 跳过WAV文件头
+
+        HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
+
+        // 初始填充两个缓冲区
+        f_read(fp, buffer_A, size*2, &count); // 读取第一个缓冲区的数据
+        f_read(fp, buffer_B, size*2, &count); // 读取第二个缓冲区的数据
+
+        int j = 0;
+        uint8_t current_buffer = 0; // 0表示buffer_A, 1表示buffer_B
+
+        HAL_TIM_Base_Start_IT(&htim1);
+
+        while(1) {
+            if(timer6flag) {
+                if(j >= size) {
+                    // 当前缓冲区播放完毕，切换到另一个缓冲区
+                    if(current_buffer == 0) {
+                        // 异步读取下一块数据到buffer_A
+                        if(f_eof(fp) == 0) {
+                            f_read(fp, buffer_A, size*2, &count);
+                        }
+                        current_buffer = 1;
+                    } else {
+                        // 异步读取下一块数据到buffer_B
+                        if(f_eof(fp) == 0) {
+                            f_read(fp, buffer_B, size*2, &count);
+                        }
+                        current_buffer = 0;
+                    }
+                    j = 0;
+
+                    // 检查是否到达文件末尾
+                    if(f_eof(fp) && j >= size) {
+                        break;
+                    }
+                }
+
+                // 播放当前样本
+                uint16_t dac_val;
+                if(current_buffer == 0) {
+                    dac_val = (uint16_t)((buffer_A[j] + 32768) >> 4);
+                } else {
+                    dac_val = (uint16_t)((buffer_B[j] + 32768) >> 4);
+                }
+
+                HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, dac_val);
+                j++;
+                timer6flag = 0;
+            }
+        }
+    }
+    f_close(fp);
+}
+
+void play_sine_table(void) {
+    HAL_TIM_Base_Start_IT(&htim1);
+    HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
+    while(1){
+    for (uint32_t i = 0; i < SINE_TABLE_SIZE; ++i) {
+        while (!timer6flag);  // Wait for timer flag
+        HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, sine_table[i]);
+        timer6flag = 0;
+    }
+    }
+
+    HAL_DAC_Stop(&hdac1, DAC_CHANNEL_1);
+    HAL_TIM_Base_Stop_IT(&htim1);
 }
 
 /* USER CODE END 0 */
@@ -117,7 +232,6 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -126,42 +240,45 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SDMMC1_SD_Init();
   MX_FATFS_Init();
+  MX_USART3_UART_Init();
   MX_DAC1_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
+
+  //HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
+  //HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 2000);
+  //HAL_Delay(5);
+
+  uint8_t rxData[3]; // 4 bytes of data received from master board
+  uint8_t volume;
+  uint8_t bpm;
+  uint8_t state = 1;
   FATFS FatFs;
   FIL Fil;
   FRESULT res;
-
-
-  int16_t size = 2048; // size of buffer;
-  int16_t buffer_A[size];
-  int16_t buffer_B[size];
-
   int8_t index;
   index = 3;
+
   res = f_mount(&FatFs, SDPath, 1);
-   if(res == FR_OK){
-	   playwav(&Fil , res , index); // play wav file with No. index
-  	      }
-
-   res = f_mount(NULL, "", 0);
-
+  if(res == FR_OK){
+	  playwav(&Fil , res , index); // play wav file with No. index
+  }
+  res = f_mount(NULL, "", 0);
 
 
+  //play_sine_table();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -199,8 +316,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 2;
-  RCC_OscInitStruct.PLL.PLLN = 24;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 12;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV4;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV4;
@@ -296,6 +413,121 @@ static void MX_SDMMC1_SD_Init(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 32;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 32;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_OC_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_TIMING;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.BreakFilter = 0;
+  sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+  sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+  sBreakDeadTimeConfig.Break2Filter = 0;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 9600;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -341,7 +573,6 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-
 /* USER CODE END 4 */
 
 /**
@@ -353,9 +584,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-  }
+  while (1){}
   /* USER CODE END Error_Handler_Debug */
 }
 
